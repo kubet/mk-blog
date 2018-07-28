3c5c9c647911b3ad8f7b44186c570ddcffc52c59
vukasin <vukasinkubet@gmail.com>
2018-07-28 13:43:50 +0200

    Error Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: 'Manufaktura koda',
     siteUrl: 'https://manufakturakoda.com',
     author: 'mk',
+    twitter: '/',
     adsense: '',
   },
   pathPrefix: '/',
