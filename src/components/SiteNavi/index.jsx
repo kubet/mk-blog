f284d33949f74fb18d7edfe34ea5aa1d78e9a9a5
vukasin <vukasinkubet@gmail.com>
2018-07-28 13:18:34 +0200

    Nav bar update
    nav bar je sada responzivan

--- src/components/SiteNavi/index.jsx
+++ src/components/SiteNavi/index.jsx
@@ -8,14 +8,25 @@ class SiteNavi extends React.Component {
     const { location, title } = this.props
     return (
       <nav class="navbar navbar-expand-lg lex-column flex-md-row navbar-light bg-light">
+                <div className="nlogo">
                 <Link className="" to="/">
-            <h1 className="navbar-brand mt-1"id="logo">MANUFAKTURA <span id="crvena">KODA</span></h1>
+            <h1 className="navbar-brand mt-1"id="desktop">MANUFAKTURA <span id="crvena">KODA</span></h1>
+            <h1 className="navbar-brand"id="mobile">M<span id="crvena">K</span></h1>
           </Link>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="container d-flex pr-7" id="navbarNavDropdown">
-    <ul class="navbar-nav ml-auto">
+          </div>
+          <input type="checkbox" id="nav" class="hidden" />
+                      <label for="nav" class="nav-btn">
+                            <i></i>
+                            <i></i>
+                            <i></i>
+                      </label>
+  <div class="nav-wrapper">
+  <div className="nlogo">
+                <Link className="" to="/">
+            <h1 className="navbar-brand"id="mobile">M<span id="crvena">K</span></h1>
+          </Link>
+          </div>
+    <ul class="navbar-nav ml-auto pl-7">
     <li
                 className={
                   location.pathname === '/' ? 'nav-item active' : 'nav-item'
