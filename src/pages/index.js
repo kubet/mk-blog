1723a7b87a765621fc3813941b8c946ae06abf49
vukasin <vukasinkubet@gmail.com>
2018-07-29 13:19:49 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,10 @@ import LazyLoad from 'react-lazyload'
 import Onama from '../components/onama'
 import SitePost from '../components/SitePost'
 
+import './styles.scss'
+const pathPrefix =
+      process.env.NODE_ENV === 'development' ? '' : __PATH_PREFIX__
+
 class BlogIndex extends React.Component {
   render() {
     const pageLinks = []
@@ -30,21 +34,49 @@ class BlogIndex extends React.Component {
 
     return (
       <div>
-            <Onama />
+            <html style={{ backgroundImage: `url(${pathPrefix + '/img/obg.png'})` }} id="onama">
+    <div>
+    <div className="container-fluid" >
+    </div>
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+		</div>
+	</div>
+	<div class="row pt-13">
+		<div class="col-md-6">
+		</div>
+		<div class="col-md-6">
+			<h2 id="h2">
+				Zasto Bas Mi?
+			</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+		</div>
+		<div class="col-md-6">
+			<h1 id="h1">
+				Lorem & Lipsum
+			</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+		</div>
+		<div class="col-md-6">
+			<p id="p">
+				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. <br/> Aliquam eget sapien sapien. Curabitur in metus urna. <br/>  In hac habitasse platea dictumst,In am us.
+			</p>
+			<Link to="/kontakt" className="nav-link">
+			<button type="button" id="joso" class="btn btn-outline-dark">KONTAKT</button>
+			</Link>
+		</div>
+	</div>
+</div>
+    </div>
+    </html>
         <Helmet
-          title={get(site, 'title')}
-          meta={[
-            { name: 'twitter:card', content: 'summary' },
-            { name: 'twitter:site', content: `@${get(site, 'twitter')}` },
-            { property: 'og:title', content: get(site, 'title') },
-            { property: 'og:type', content: 'website' },
-            { property: 'og:description', content: get(site, 'description') },
-            { property: 'og:url', content: get(site, 'url') },
-            {
-              property: 'og:image',
-              content: `${get(site, 'url')}/img/profile.jpg`,
-            },
-          ]}
         />
         {pageLinks}
       </div>
